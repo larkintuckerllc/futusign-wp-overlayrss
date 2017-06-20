b5d0bc56a6ac2cf32b61b066a64233cfc31bd2d6
John Tucker <john@larkintuckerllc.com>
2017-06-20 17:22:16 -0400

    extracting acf

--- public/futusign-overlayrss-template.php
+++ public/futusign-overlayrss-template.php
@@ -12,8 +12,14 @@ $fs_or_args = array(
 $fs_or_loop = new WP_Query( $fs_or_args );
 while ( $fs_or_loop->have_posts() ) {
 	$fs_or_loop->the_post();
-	// TODO: GET ADV CUST
-	$fs_or_title = get_the_title();
+	$fs_or_url = get_field('url');
+	$fs_or_cycling = get_field('cycling');
+	$fs_or_polling = get_field('polling');
+	$fs_or_size = get_field('size');
+	$fs_or_theme = get_field('theme');
+	$fs_or_publication_dates = get_field('publication_dates');
+	$fs_or_maximum_age =  get_field('maximum_age');
+	$fs_or_parse = get_field('parse');
 }
 wp_reset_query();
 // OUTPUT
@@ -27,6 +33,15 @@ header( 'Cache-Control: no-cache, no-store, must-revalidate');
   <title>futusign Overlay RSS</title>
 </head>
 <body>
-	<?php echo $fs_or_title; ?>
+	<script>
+		var fsOrURL = "<?php echo $fs_or_url; ?>";
+		var fsOrCycling = <?php echo $fs_or_cycling ?>;
+		var fsOrPolling = <?php echo $fs_or_polling ?>;
+		var fsOrSize = <?php echo $fs_or_size ?>;
+		var fsOrTheme = "<?php echo $fs_or_theme ?>";
+		var fsOrPublicationDates = <?php echo $fs_or_publication_dates ?>;
+		var fsOrMaximumAge = fsOrPublicationDates ? Number("<?php echo $fs_or_maximum_age ?>") : null;
+		var fsOrParse = "<?php echo $fs_or_parse ?>";
+	</script>
 </body>
 </html>
