d6cf45defec40e5b50c4b4614fdfad8a95d6d54f
John  Tucker <john@larkintuckerllc.com>
2018-03-24 22:13:55 -0400

    FUCK IT ALL

--- public/rss/src/components/App/Frame/index.jsx
+++ public/rss/src/components/App/Frame/index.jsx
@@ -5,7 +5,6 @@ import { getWidget } from '../../../apis/widget';
 
 const Frame = ({ children, empty, fetchItemsErrorMessage }) => {
   const { LIGHT, SIZE } = getWidget();
-  window.console.log(SIZE);
   let className;
   if (empty || fetchItemsErrorMessage !== null) {
     className = styles.rootEmpty;
