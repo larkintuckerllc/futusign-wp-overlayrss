f1b517d5dcd6b4ecbbdb00bc6b1aa42316f64b93
John  Tucker <john@larkintuckerllc.com>
2019-01-13 06:11:22 -0500

    Maybe Done

--- public/rss/src/apis/feed.js
+++ public/rss/src/apis/feed.js
@@ -1,7 +1,10 @@
-import jsonp from 'jsonp';
 import moment from 'moment';
+import Parser from 'rss-parser';
+import { SITE_URL } from '../strings';
+import { getPretty } from './base';
 import { getWidget } from './widget';
 
+const parser = new Parser();
 let drift = 0;
 window.addEventListener('message', (message) => {
   switch (message.data.type) {
@@ -13,6 +16,7 @@ window.addEventListener('message', (message) => {
     default:
   }
 });
+
 // eslint-disable-next-line
 export const get = () => {
   const {
@@ -24,58 +28,32 @@ export const get = () => {
     TITLE_PARSE,
     URL,
   } = getWidget();
-  const REG = /\?/;
-  const version = Date.now().toString();
-  const url = REG.test(URL)
-  ? `${URL}&fs-or-version=${version}`
-  : `${URL}?fs-or-version=${version}`;
-  // TODO: NEED TO PRE-ENDCODE URL
-  const TIMEOUT = 10 * 1000;
-  const RE_DESCRIPTION = new RegExp(DESCRIPTION_PARSE, 'm');
-  const RE_TITLE = new RegExp(TITLE_PARSE, 'm');
-  const YQL_ENDPOINT = 'https://query.yahooapis.com/v1/public/yql';
-  const YQL_SELECT = encodeURIComponent('select pubDate, title, description ');
-  const YQL_FROM = encodeURIComponent('from rss ');
-  const YQL_WHERE = encodeURIComponent(`where url="${url}"`);
-  const YQL_URL = `${YQL_ENDPOINT}?q=${YQL_SELECT}${YQL_FROM}${YQL_WHERE}&format=json`;
-  window.parent.postMessage({
-    type: 'MSG_TIME',
-  }, '*');
   if (URL === null) {
     return Promise.reject({
       message: '400',
     });
   }
-  return new Promise((resolve, reject) => {
-    jsonp(YQL_URL, { timeout: TIMEOUT }, (err, data) => {
-      if (err !== null) {
-        reject({
-          message: '500',
-        });
-        return;
-      }
-      if (
-        data.query === undefined ||
-        data.query.results === undefined
-      ) {
-        reject({
-          message: '400',
-        });
-        return;
-      }
-      let items;
-      if (data.query.results === null) {
-        items = [];
-      } else if (!Array.isArray(data.query.results.item)) {
-        items = [data.query.results.item];
-      } else {
-        items = data.query.results.item;
-      }
-      let maxAgeM = null;
-      if (PUB_DATES) {
-        maxAgeM = moment(Date.now() - drift).subtract(MAX_AGE, 's');
-      }
-      let transformed = items.map((o, i) => {
+  const RE_DESCRIPTION = new RegExp(DESCRIPTION_PARSE, 'm');
+  const RE_TITLE = new RegExp(TITLE_PARSE, 'm');
+  const RE_STRIP = /^\s*(.*)\s*/;
+  const ENDPOINT = getPretty() ?
+    `${SITE_URL}fs-or-rss-endpoint?futusign_or_rss_url=${URL}` :
+    `${SITE_URL}?fs-or-rss-endpoint?futusign_or_rss_url=${URL}`;
+  window.parent.postMessage({
+    type: 'MSG_TIME',
+  }, '*');
+  let maxAgeM = null;
+  if (PUB_DATES) {
+    maxAgeM = moment(Date.now() - drift).subtract(MAX_AGE, 's');
+  }
+  return fetch(ENDPOINT)
+    .then((response) => {
+      if (!response.ok) throw new Error();
+      return response.text();
+    })
+    .then(xml => parser.parseString(xml))
+    .then((json) => {
+      let transformed = json.items.map((o, i) => {
         const value = {
           id: i,
         };
@@ -83,17 +61,23 @@ export const get = () => {
         if (TITLE && o.title === undefined) return null;
         if (TITLE) {
           let title = o.title;
-          const match = RE_TITLE.exec(title);
+          let match = RE_STRIP.exec(title);
+          if (match === null) return null;
+          title = match[1];
+          match = RE_TITLE.exec(title);
           if (match === null) return null;
           title = match[1];
           if (title === '') return null;
           value.title = title;
         }
         // DESCRIPTION
-        if (DESCRIPTION && o.description === undefined) return null;
+        if (DESCRIPTION && o.content === undefined) return null;
         if (DESCRIPTION) {
-          let description = o.description;
-          const match = RE_DESCRIPTION.exec(description);
+          let description = o.content;
+          let match = RE_STRIP.exec(description);
+          if (match === null) return null;
+          description = match[1];
+          match = RE_DESCRIPTION.exec(description);
           if (match === null) return null;
           description = match[1];
           if (description === '') return null;
@@ -112,7 +96,6 @@ export const get = () => {
       });
       transformed = transformed.filter(o => o !== null);
       if (PUB_DATES) transformed = transformed.sort((a, b) => b.pubDate - a.pubDate);
-      resolve(transformed);
+      return transformed;
     });
-  });
 };
